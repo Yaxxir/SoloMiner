6c589be7f3ac6d8150d657c18d0008e128e51202
Yaxxir <160383327+Yaxxir@users.noreply.github.com>
2024-02-19 10:45:43 +0530

    Update SoloMinerV2.py

--- SoloMinerV2.py
+++ SoloMinerV2.py
@@ -24,7 +24,7 @@ def timer() :
 
 # Changed this Address And Insert Your BTC Wallet
 
-address = '16p9y6EstGYcnofGNvUJMEGKiAWhAr1uR8' 
+address = ‘bc1q2f5m64rt50wlhfp9qmrx80s7nr49gqhgceelgc' 
 
 print(Back.BLUE , Fore.WHITE , 'BTC WALLET:' , Fore.BLACK , str(address) , Style.RESET_ALL)
 
